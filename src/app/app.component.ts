4fe54606d103fac36c60171993c51477418b4816
NairatTasneem <tasneemnairat1117@gmail.com>
2022-08-17 00:08:06 +0300

    get user data and validate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
+import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+export interface userVm {
+  id:Number;
+  name:String;
+  email:String;
+  age:Number;
+  password:String;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UserProject';
+  // name:string ="Tasneem";
+  //x:Xtype={name="tasnem"} ->object
+  //x:string[]=["assets"] ->array 
+
+  UserList:userVm[]=[
+
+  ]
+  newUser:userVm= {id:0 , name:'' ,email:'' , age:0 ,password:''};
+  i:number =this.UserList.length;
+  AddUser(form:NgForm){
+    //this.UserList.push({...this.newUser})
+   // let i=0;
+    if(!form.form.valid){
+    form.form.markAllAsTouched();
+    }else{
+    this.UserList.push({id:this.i,name:this.newUser.name,age:this.newUser.age,email:this.newUser.email,password:this.newUser.password});
+    this.i++;
+  }
+  }
+  deleteUser(user: userVm){
+    let i= this.UserList.indexOf(user);
+    if (i>=0){
+      this.UserList.splice(i,1);
+    }
+  }  
 }
